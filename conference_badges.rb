77df1a3356052e58e157c7a35e31c89e61381eca
Devone Austin <devone.austin@gmail.com>
2020-04-16 17:37:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,5 @@ end
 def printer(attendees)
   badges_and_room_assignments = []
   batch_badge_creator.each {|badge| badges_and_room_assignments << batch_badge_creator(attendees)}
-  badges_and_room_assignments
+  badges_and_room_assignments << assign_rooms(attendees)
 end
