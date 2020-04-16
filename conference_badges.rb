1136627d9671fc6924ddf8b5faab24970928afe5
Devone Austin <devone.austin@gmail.com>
2020-04-16 17:22:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each {|attendee| badge_messages << badge_maker(attendee)}
+  badge_messages
+end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index {|attendees, room| rooms << "Hello, #{attendees}! You'll be assigned to room #{room + 1}!"}
+  rooms
+end
+
+def printer(attendees)
+  badges_and_room_assignments = []
+end
